e702ceff0cca5d7e027d084a8fbb8d01056c8867
JustW <vuav2003@gmail.com>
2023-08-15 09:08:37 +0700

    display map

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
